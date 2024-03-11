8009fd54174b6a3fe53ca0505990e2ca5785e334
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-03-11 13:15:11 +0100

    comment styles

--- src/components/comments/Comment.tsx
+++ src/components/comments/Comment.tsx
@@ -1,4 +1,5 @@
 import { TComment } from "../../types/comments"
+import { usersImages } from "../../constants/usersImages"
 
 type CommentProps = {
   comment: TComment;
@@ -6,7 +7,14 @@ type CommentProps = {
 
 function Comment({ comment }: CommentProps) {
   return (
-    <div className="comment">Comment</div>
+    <div className="comment">
+      <div className="comment__top">
+        <img className="comment__top-img" src={usersImages[comment.user.image.png]} alt="" />
+        <span className="comment__top-username">{comment.user.username}</span>
+        <span className="comment__top-created-at">{comment.createdAt}</span>
+      </div>
+      <p className="comment__content">{comment.content}</p>
+    </div>
   )
 }
 
---
+++ src/constants/usersImages.ts
@@ -0,0 +1,15 @@
+import amyrobsonImg from "../assets/avatars/image-amyrobson.png";
+import juliusomoImg from "../assets/avatars/image-juliusomo.png";
+import maxblagunImg from "../assets/avatars/image-maxblagun.png";
+import ramsesmironImg from "../assets/avatars/image-ramsesmiron.png";
+
+type UsersImages = {
+    [key: string]: string;
+}
+
+export const usersImages: UsersImages = {
+    "./images/avatars/image-amyrobson.png": amyrobsonImg,
+    "./images/avatars/image-juliusomo.png": juliusomoImg,
+    "./images/avatars/image-maxblagun.png": maxblagunImg,
+    "./images/avatars/image-ramsesmiron.png": ramsesmironImg,
+}
