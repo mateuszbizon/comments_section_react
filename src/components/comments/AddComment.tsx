baafa72094cb1a0206adc50540d2d3a4798b29c3
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-03-13 14:26:26 +0100

    adding new comments

--- src/components/comments/AddComment.tsx
+++ src/components/comments/AddComment.tsx
@@ -1,9 +1,29 @@
+import { useState } from "react"
 import AddCommentForm from "../forms/AddCommentForm"
+import { useSetRecoilState } from "recoil";
+import { commentsState } from "../../state/atoms/commentsState";
+import usersData from "../../data/data.json";
 
 function AddComment() {
+  const [newComment, setNewComment] = useState("");
+  const setComment = useSetRecoilState(commentsState)
+
+  function addComment() {
+    setComment(prev => {
+      return [...prev, {
+        id: Math.floor(Math.random() * 10000),
+        content: newComment,
+        user: usersData.currentUser,
+        createdAt: "today",
+        score: 0,
+        replies: []
+      }]
+    })
+  }
+
   return (
     <div className="add-comment">
-      <AddCommentForm />
+      <AddCommentForm newComment={newComment} setNewComment={setNewComment} submitFunction={addComment} />
     </div>
   )
 }
--- src/components/forms/AddCommentForm.tsx
+++ src/components/forms/AddCommentForm.tsx
@@ -1,18 +1,38 @@
+import { ChangeEvent, FormEvent } from "react";
 import { usersImages } from "../../constants/usersImages";
 import usersData from "../../data/data.json";
 
-function AddCommentForm() {
+type AddCommentFormProps = {
+	newComment: string;
+	setNewComment: (newComment: string) => void;
+	submitFunction: () => void;
+}
+
+function AddCommentForm({ newComment, setNewComment, submitFunction }: AddCommentFormProps) {
+	function handleInputChange(e: ChangeEvent<HTMLTextAreaElement>) {
+		setNewComment(e.target.value)
+	}
+
+	function handleSubmit(e: FormEvent) {
+		e.preventDefault();
+
+		if (newComment.length == 0) return;
+
+		submitFunction();
+		setNewComment("");
+	}
+
 	return (
-		<form className='add-comment-form'>
+		<form className='add-comment-form' onSubmit={handleSubmit}>
 			<div className='add-comment-form__mobile'>
-				<textarea className='add-comment-form__textarea' rows={5} placeholder="Add a comment..."></textarea>
+				<textarea className='add-comment-form__textarea' value={newComment} onChange={handleInputChange} rows={5} placeholder="Add a comment..."></textarea>
 				<div className='add-comment-form__mobile-row'>
 					<img
 						className='add-comment-form__img'
 						src={usersImages[usersData.currentUser.image.png]}
 						alt=''
 					/>
-					<button className='add-comment-form__send-btn'>send</button>
+					<button type="submit" className='add-comment-form__send-btn'>send</button>
 				</div>
 			</div>
 		</form>
