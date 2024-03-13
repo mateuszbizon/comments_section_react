859ffd4f08af19ff94a19f2f9eb32ab1dc3094bc
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-03-13 15:48:15 +0100

    replying to comments

--- src/components/comments/Comment.tsx
+++ src/components/comments/Comment.tsx
@@ -6,13 +6,42 @@ import EditButton from "../buttons/EditButton";
 import DeleteButton from "../buttons/DeleteButton";
 import Replies from "./Replies";
 import useCurrentuser from "../../hooks/useCurrentuser";
+import AddCommentForm from "../forms/AddCommentForm";
+import { useState } from "react";
+import { useSetRecoilState } from "recoil";
+import { commentsState } from "../../state/atoms/commentsState";
+import usersData from "../../data/data.json";
 
 type CommentProps = {
   comment: CommentType;
 }
 
 function Comment({ comment }: CommentProps) {
-  const { isCurrentUser } = useCurrentuser(comment.user.username)
+  const [newReply, setNewReply] = useState("");
+  const setReply = useSetRecoilState(commentsState);
+  const { isCurrentUser } = useCurrentuser(comment.user.username);
+
+  function addReply() {
+    setReply(prev => {
+      return prev.map(commentItem => {
+        if (commentItem.id === comment.id) {
+          return {
+            ...commentItem,
+            replies: [...commentItem.replies, {
+              id: Math.floor(Math.random() * 10000), 
+              content: newReply, 
+              createdAt: "today", 
+              score: 0, 
+              replyingTo: comment.user.username, 
+              user: usersData.currentUser,
+            }]
+          };
+        }
+
+        return commentItem;
+      });
+    })
+  }
 
   return (
     <div className="comment-container">
@@ -30,6 +59,9 @@ function Comment({ comment }: CommentProps) {
             <EditButton />
           </>
         ) : <ReplyButton />}
+      </div>
+      <div className="comment">
+        <AddCommentForm newComment={newReply} setNewComment={setNewReply} submitFunction={addReply} />
       </div>
       {comment.replies.length > 0 && (
         <Replies replies={comment.replies} />
